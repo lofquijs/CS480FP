bbb95fb34767eaf332ab6c2af5d5438b5cc2b5ce
Andrew Hansen <123025570+HansenSAndrew@users.noreply.github.com>
2025-05-05 03:02:48 -0400

    Finished MapMatcher Implementation
    I believe I have finished how we would find the "closest geographic shapes." The next step is to cipher through the list returned and find the specific road to use.

--- FinalProject/src/geography/MapMatcher.java
+++ FinalProject/src/geography/MapMatcher.java
@@ -1,22 +1,156 @@
 package geography;
 
+import java.awt.Shape;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.PathIterator;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.Map;
+import gui.CartographyDocument;
 
-// TODO: I know I didn't really do anything, but I am still thinking of how to implement this.
-// 		 My general idea is to have this class be called in the GeographicShapesReader method
-// 		 read(). 
-// 		 Ex: read(MapMatcher mm);
-//		 However, I'm having second thoughts on this. How am I going to distinguish which shape
-//       goes in what bucket? There isn't a label associated with these things like on the exam.
-//		 Using the lat and long would be stupid because they are floats. Perhaps I try using
-//		 PathIterator, and then figure something out? Maybe use the Geocoder when doing this work.
-//       I don't know. It's 10:30, and I was editing my Korean movie. It's time to start drinking
-//		 this HARD Arizona Iced Tea!
-public class MapMatcher {
-	private ArrayList<LinkedList<GeographicShape>> bucket;
+/**
+ * Class used for map matching.
+ * 
+ * @author Andrew Hansen
+ * 
+ * 
+ * This work complies with the JMU Honor Code.
+ */
+public class MapMatcher 
+{
+ // These values are used to create the buckets. I hate global variables 
+ // as much as the next guy, but it is what it is.
+  private static double MAX_X = Double.MIN_VALUE;
+  private static double MAX_Y = Double.MIN_VALUE; 
+	private static double MIN_X = Double.MAX_VALUE;
+	private static double MIN_Y = Double.MAX_VALUE;
+	private static double RATIO_X;
+	private static double RATIO_Y;
+	private static int GAP = 100; // How big the grid is (i.e. 10 x 10)
+	                              // We can experiment with this value.
 	
-	public MapMatcher() {
+	// This is the buckets itself. It is quite confusing, but all you need to know is that
+	// it is a HashMap inside of another HashMap. You use the RATIO values to access it.
+	// This comes from instruction of Dr. Bernstein. This is basically a 2D Array (Grid).
+	// Is there a better way of doing this? Probably. Is it done? Hopefully.
+	private HashMap<Double, HashMap<Double, 
+	    LinkedList<GeographicShape>>> buckets = new HashMap<>();
+	
+	/**
+	 * Creates the MapMatcher object and populates the buckets.
+	 * @param document -> Document that contains all geographic shapes.
+	 */
+	public MapMatcher(final CartographyDocument<GeographicShape> document) 
+	{
+	  // Used to compare km values with future values and have the 
+	  // ability to access the geographic shape.
+		HashMap<double[], GeographicShape> allCoords = new HashMap<>();
 		
+		// Very similar to Geocode class implementation by Dr. Bernstein
+		Iterator<GeographicShape> iter = document.iterator();
+		AffineTransform identity = new AffineTransform();
+		while (iter.hasNext())
+		{
+			GeographicShape gshape = iter.next();
+			Shape s = gshape.getShape();
+			PathIterator pi = s.getPathIterator(identity);
+			while(!pi.isDone())
+			{
+				double[] coords = new double[2];
+				pi.currentSegment(coords);
+				allCoords.put(coords, gshape);
+				if (MAX_X < coords[0]) MAX_X = coords[0];
+				if (MAX_Y < coords[1]) MAX_Y = coords[1];
+				if (MIN_X > coords[0]) MIN_X = coords[0];
+				if (MIN_Y > coords[1]) MIN_Y = coords[1];
+				pi.next();
+			}
+		}
+		
+		RATIO_X = (MAX_X - MIN_X) / GAP;
+		RATIO_Y = (MAX_Y - MIN_Y) / GAP;
+		System.out.println(MIN_Y);
+		System.out.println(MIN_X);
+		
+		
+		// Initialize buckets
+		for (int i = 1; i <= GAP; i++)
+		{
+			double x = MIN_X + (i * RATIO_X);
+			buckets.put(x, new HashMap<>());
+			for (int j = 1; j <= GAP; j++)
+			{
+				double y = MIN_Y + (j * RATIO_Y);
+				buckets.get(x).put(y, new LinkedList<>());
+			}
+		}
+		
+		// Populate buckets
+		for (Map.Entry<double[], GeographicShape> entry : allCoords.entrySet())
+		{
+			double[] coord = entry.getKey();
+			double coordX = coord[0];
+			double coordY = coord[1];
+			
+			for (int i = 1; i <= GAP; i++)
+			{
+				double placementX = (RATIO_X * i) + MIN_X;
+				if (coordX <= placementX)
+				{
+					for (int j = 1; j <= GAP; j++)
+					{
+						double placementY = (RATIO_Y * j) + MIN_Y;
+						if (coordY <= placementY)
+						{
+							buckets.get(placementX)
+							       .get(placementY)
+							       .add(entry.getValue());
+							break;
+						}
+					}
+					break;
+				}
+			}
+		}
 	}
+	
+	/**
+	 * Finds the closest Geographic Shapes to the current location.
+	 * @param km -> Current location in kilometers
+	 * @return Geographic Shapes nearest desired km
+	 */
+	public LinkedList<GeographicShape> getClosestGeographicShapes(final double[] km)
+	{
+		double x = km[0];
+		double y = km[1];
+		
+		ArrayList<Double> listX = new ArrayList<>(buckets.keySet());
+		Collections.sort(listX);
+		if (x > MAX_X || x < MIN_X) 
+			return null;
+		for (Double placementX : listX)
+		{
+			if (x <= placementX) 
+			{
+				HashMap<Double, LinkedList<GeographicShape>> mapY 
+				    = buckets.get(placementX);
+				ArrayList<Double> listY = new ArrayList<>(mapY.keySet());
+				Collections.sort(listY);
+				if (y > MAX_Y || y < MIN_Y) 
+					return null;
+				for (double placementY : listY)
+				{
+					if (y <= placementY)
+						return buckets.get(placementX).get(placementY);
+				}
+			}
+		}
+		
+		// IT SHOULD NEVER REACH HERE!
+		return null;
+	}
+	
 }
---
+++ FinalProject/src/testing/MapMatcherTest.java
@@ -0,0 +1,58 @@
+package testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.LinkedList;
+
+import org.junit.jupiter.api.Test;
+import geography.AbstractMapProjection;
+import geography.ConicalEqualAreaProjection;
+import geography.GeographicShape;
+import geography.GeographicShapesReader;
+import geography.MapMatcher;
+import gui.CartographyDocument;
+
+class MapMatcherTest {
+
+	@Test
+	void TestConstructor() throws FileNotFoundException {
+		InputStream isgeo = new FileInputStream(new File("rockingham-streets.geo"));
+	    AbstractMapProjection proj = new ConicalEqualAreaProjection(-96.0, 37.5, 29.5, 45.5);
+	    GeographicShapesReader gsReader = new GeographicShapesReader(isgeo, proj);
+	    CartographyDocument<GeographicShape> geographicShapes = gsReader.read();
+	    MapMatcher mm = new MapMatcher(geographicShapes);
+	    double[] km = {1456.232, 240.0};
+	    LinkedList<GeographicShape> shapes = mm.getClosestGeographicShapes(km);
+	    assertEquals(shapes.getClass(), LinkedList.class);
+	}
+	
+	@Test
+	void TestEdgeCases() throws FileNotFoundException
+	{
+		InputStream isgeo = new FileInputStream(new File("rockingham-streets.geo"));
+	    AbstractMapProjection proj = new ConicalEqualAreaProjection(-96.0, 37.5, 29.5, 45.5);
+	    GeographicShapesReader gsReader = new GeographicShapesReader(isgeo, proj);
+	    CartographyDocument<GeographicShape> geographicShapes = gsReader.read();
+	    MapMatcher mm = new MapMatcher(geographicShapes);
+	    
+	    // NULLS
+	    double[] km1 = {Double.MAX_VALUE, Double.MAX_VALUE};
+	    double[] km2 = {1500.0, Double.MIN_VALUE};
+	    double[] km3 = {Double.MIN_VALUE, Double.MAX_VALUE};
+	    double[] km4 = {1500.0, Double.MAX_VALUE};
+	    assertNull(mm.getClosestGeographicShapes(km1));
+	    assertNull(mm.getClosestGeographicShapes(km2));
+	    assertNull(mm.getClosestGeographicShapes(km3));
+	    assertNull(mm.getClosestGeographicShapes(km4));
+	    
+	    // MAX/MINS
+	    double[] maxKM = {1501.9431023788957, 281.56193744751045};
+	    double[] minKM = {1438.2523713927162, 214.37637700137842};
+	    assertEquals(mm.getClosestGeographicShapes(maxKM).getClass(), LinkedList.class);
+	    assertEquals(mm.getClosestGeographicShapes(minKM).getClass(), LinkedList.class);
+	}
+
+}
