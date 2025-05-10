2336aa141dc089e2ef62df8190677322f7645493
mpuckett7 <147020841+mpuckett7@users.noreply.github.com>
2025-05-10 12:38:48 -0400

    Updated app

--- FinalProject/src/app/FPApp.java
+++ FinalProject/src/app/FPApp.java
@@ -49,270 +49,284 @@ import gui.StreetSegmentCartographer;
 /**
  * The application for the final project of Personal Navigation Systems.
  * 
- * @author Mason Puckett, Andrew Hansen, Jackson Lofquist, Dakota Lawson, Prof. David Bernstein
+ * @author Mason Puckett, Andrew Hansen, Jackson Lofquist, Dakota Lawson, Prof.
+ *         David Bernstein
  * @version 1.0
  */
-public class FPApp
-    implements ActionListener, Runnable, StreetSegmentObserver, PropertyChangeListener
-{
-  private static final int SET_DESTINATION = 0;
-  private static final int SET_ORIGIN = 1;
-  private static final int CALCULATE_PATH = 2;
+public class FPApp implements ActionListener, Runnable, StreetSegmentObserver, PropertyChangeListener {
+	private static final int SET_DESTINATION = 0;
+	private static final int SET_ORIGIN = 1;
+	private static final int CALCULATE_PATH = 2;
 
-  private static final String CALCULATE = "Calculate";
-  private static final String EXIT = "Exit";
-  private static final String DESTINATION = "Destination";
-  private static final String ORIGIN = "Origin";
-  private static final String ASTAR = "A*";
-  private static final String BELLMANFORD = "BellmanFord";
-  private static final String DIJKSTRA = "Dijkstra";
-  private static final String CORRECTING = "Correcting";
+	private static final String CALCULATE = "Calculate";
+	private static final String EXIT = "Exit";
+	private static final String DESTINATION = "Destination";
+	private static final String ORIGIN = "Origin";
+	private static final String ASTAR = "A*";
+	private static final String BELLMANFORD = "BellmanFord";
+	private static final String DIJKSTRA = "Dijkstra";
+	private static final String CORRECTING = "Correcting";
+	private static final String HEAP = "Heap";
+	private static final String LIST = "List";
+	private static final String NEWEST = "Newest";
+	private static final String OLDEST = "Oldest";
 
-  private static final String TAB = "\t";
+	private static final String TAB = "\t";
 
-  private CartographyPanel<StreetSegment> panel;
-  private DynamicCartographyPanel<StreetSegment> dynamicPanel;
-  private CartographyDocument<StreetSegment> document;
-  private GeocodeDialog dialog;
-  private int mode;
-  private JFrame frame;
-  private ShortestPathAlgorithm alg;
-  private PermanentLabelManager pLabels;
-  private CandidateLabelManager cLabels;
-  private PathFindingWorker task;
-  private StreetSegment originSegment, destinationSegment;
-  private StreetNetwork network;
-  private JTextArea textArea;
+	private CartographyPanel<StreetSegment> panel;
+	private DynamicCartographyPanel<StreetSegment> dynamicPanel;
+	private CartographyDocument<StreetSegment> document;
+	private GeocodeDialog dialog;
+	private int mode;
+	private JFrame frame;
+	private ShortestPathAlgorithm alg;
+	private PermanentLabelManager pLabels;
+	private CandidateLabelManager cLabels;
+	private PathFindingWorker task;
+	private StreetSegment originSegment, destinationSegment;
+	private StreetNetwork network;
+	private JTextArea textArea;
 
-  /**
-   * Default constructor.
-   */
-  public FPApp()
-  {
-	  textArea = new JTextArea();
-    // nope
-  }
+	/**
+	 * Default constructor.
+	 */
+	public FPApp() {
+		textArea = new JTextArea();
+		// nope
+	}
 
-  @Override
-  public void run()
-  {
-    try
-    {
-      // CHOOSE A .geo FILE
-      InputStream isgeo = new FileInputStream(new File("rockingham-streets.geo"));
-      // InputStream isgeo = new FileInputStream(new File("virginia-streets.geo"));
-      AbstractMapProjection proj = new ConicalEqualAreaProjection(-96.0, 37.5, 29.5, 45.5);
-      GeographicShapesReader gsReader = new GeographicShapesReader(isgeo, proj);
-      CartographyDocument<GeographicShape> geographicShapes = gsReader.read();
-      System.out.println("Read the .geo file");
+	@Override
+	public void run() {
+		try {
+			// CHOOSE A .geo FILE
+			InputStream isgeo = new FileInputStream(new File("rockingham-streets.geo"));
+			// InputStream isgeo = new FileInputStream(new File("virginia-streets.geo"));
+			AbstractMapProjection proj = new ConicalEqualAreaProjection(-96.0, 37.5, 29.5, 45.5);
+			GeographicShapesReader gsReader = new GeographicShapesReader(isgeo, proj);
+			CartographyDocument<GeographicShape> geographicShapes = gsReader.read();
+			System.out.println("Read the .geo file");
 
-      // CHOOSE A .str FILE
-      InputStream iss = new FileInputStream(new File("rockingham-streets.str"));
-      // InputStream iss = new FileInputStream(new File("virginia-streets.str"));
-      StreetsReader sReader = new StreetsReader(iss, geographicShapes);
-      Map<String, Street> streets = new HashMap<String, Street>();
-      document = sReader.read(streets);
-      System.out.println("Read the .str file");
+			// CHOOSE A .str FILE
+			InputStream iss = new FileInputStream(new File("rockingham-streets.str"));
+			// InputStream iss = new FileInputStream(new File("virginia-streets.str"));
+			StreetsReader sReader = new StreetsReader(iss, geographicShapes);
+			Map<String, Street> streets = new HashMap<String, Street>();
+			document = sReader.read(streets);
+			System.out.println("Read the .str file");
 
-      panel = new CartographyPanel<StreetSegment>(document, new StreetSegmentCartographer());
-      
-      // Use this to get simulator working. Will work on GPS itself later.
-      // dynamicPanel = new DynamicCartographyPanel<StreetSegment>(document,
-      //     new StreetSegmentCartographer(), proj);
-      frame = new JFrame("Map");
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      frame.setSize(600, 600);
+			panel = new CartographyPanel<StreetSegment>(document, new StreetSegmentCartographer());
 
-      network = StreetNetwork.createStreetNetwork(streets);
+			// Use this to get simulator working. Will work on GPS itself later.
+			// dynamicPanel = new DynamicCartographyPanel<StreetSegment>(document,
+			// new StreetSegmentCartographer(), proj);
+			frame = new JFrame("Map");
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setSize(600, 600);
 
-      JMenuBar menuBar = new JMenuBar();
-      frame.setJMenuBar(menuBar);
-      JMenuItem item;
-      JMenu menu;
+			network = StreetNetwork.createStreetNetwork(streets);
 
-      menu = new JMenu("File");
-      menuBar.add(menu);
+			JMenuBar menuBar = new JMenuBar();
+			frame.setJMenuBar(menuBar);
+			JMenuItem item;
+			JMenu menu;
 
-      item = new JMenuItem(EXIT);
-      item.addActionListener(this);
-      menu.add(item);
+			menu = new JMenu("File");
+			menuBar.add(menu);
 
-      menu = new JMenu("Geocode");
-      menuBar.add(menu);
+			item = new JMenuItem(EXIT);
+			item.addActionListener(this);
+			menu.add(item);
 
-      item = new JMenuItem(ORIGIN);
-      item.addActionListener(this);
-      menu.add(item);
-      item = new JMenuItem(DESTINATION);
-      item.addActionListener(this);
-      menu.add(item);
+			menu = new JMenu("Geocode");
+			menuBar.add(menu);
 
-      menu = new JMenu("Path");
-      menuBar.add(menu);
+			item = new JMenuItem(ORIGIN);
+			item.addActionListener(this);
+			menu.add(item);
+			item = new JMenuItem(DESTINATION);
+			item.addActionListener(this);
+			menu.add(item);
 
-      item = new JMenuItem(CALCULATE);
-      item.addActionListener(this);
-      menu.add(item);
-      
-      menu = new JMenu("Algorithm");
-      menuBar.add(menu);
-      
-      item = new JMenuItem(ASTAR);
-      item.addActionListener(this);
-      menu.add(item);
-      item = new JMenuItem(BELLMANFORD);
-      item.addActionListener(this);
-      menu.add(item);
-      item = new JMenuItem(DIJKSTRA);
-      item.addActionListener(this);
-      menu.add(item);
-      item = new JMenuItem(CORRECTING);
-      item.addActionListener(this);
-      menu.add(item);
+			menu = new JMenu("Path");
+			menuBar.add(menu);
 
-      frame.setContentPane(panel); //Switch to dynamicPanel to see the GPS Sim
-      frame.setVisible(true);
+			item = new JMenuItem(CALCULATE);
+			item.addActionListener(this);
+			menu.add(item);
 
-      Geocoder geocoder = new Geocoder(geographicShapes, document, streets);
-      dialog = new GeocodeDialog(frame, geocoder);
-      dialog.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-      dialog.addStreetSegmentObserver(this);
-      dialog.setLocation((int) frame.getBounds().getMaxX(), (int) frame.getBounds().getY());
+			menu = new JMenu("Algorithm");
+			menuBar.add(menu);
 
-      GPSSimulator gps = new GPSSimulator("rockingham.gps");
-      InputStream is = gps.getInputStream();
+			item = new JMenuItem(ASTAR);
+			item.addActionListener(this);
+			menu.add(item);
+			item = new JMenuItem(BELLMANFORD);
+			item.addActionListener(this);
+			menu.add(item);
+			item = new JMenuItem(DIJKSTRA);
+			item.addActionListener(this);
+			menu.add(item);
+			item = new JMenuItem(CORRECTING);
+			item.addActionListener(this);
+			menu.add(item);
 
-      // Setup the GPSReaderTask
-      GPSReaderTask gpsReader = new GPSReaderTask(is, "GPGGA");
-      gpsReader.addGPSObserver(dynamicPanel);
-      frame.setVisible(true);
-      gpsReader.execute();
+			menu = new JMenu("Permanent Structure");
+			menuBar.add(menu);
 
-      frame.setVisible(true);
-    }
-    catch (IOException ioe)
-    {
-      JOptionPane.showMessageDialog(frame, ioe.toString(), "Error", JOptionPane.ERROR_MESSAGE);
-    }
+			item = new JMenuItem(HEAP);
+			item.addActionListener(this);
+			menu.add(item);
+			item = new JMenuItem(LIST);
+			item.addActionListener(this);
+			menu.add(item);
 
-  }
+			menu = new JMenu("Candidate Policy");
+			menuBar.add(menu);
 
-  @Override
-  public void propertyChange(final PropertyChangeEvent evt)
-  {
-    if (evt.getPropertyName().equals("progress"))
-    {
-      System.out.println("progress...");
-    }
-    else if (evt.getPropertyName().equals("state"))
-    {
-      if (evt.getNewValue().equals(SwingWorker.StateValue.DONE))
-      {
-        try
-        {
-          Map<String, StreetSegment> path = task.get();
-          document.setHighlighted(path);
-          panel.repaint();
-          task = null;
-        }
-        catch (InterruptedException | ExecutionException e)
-        {
-          JOptionPane.showMessageDialog(frame, "Interrupted", "Exception",
-              JOptionPane.ERROR_MESSAGE);
-          e.printStackTrace();
-        }
-      }
-    }
-  }
+			item = new JMenuItem(NEWEST);
+			item.addActionListener(this);
+			menu.add(item);
+			item = new JMenuItem(OLDEST);
+			item.addActionListener(this);
+			menu.add(item);
 
-  @Override
-  public void handleStreetSegments(final List<String> segmentIDs)
-  {
-    HashMap<String, StreetSegment> highlighted = new HashMap<String, StreetSegment>();
-    for (String id : segmentIDs)
-    {
-      highlighted.put(id, document.getElement(id));
-    }
-    document.setHighlighted(highlighted);
-    panel.repaint();
+			frame.setContentPane(panel); // Switch to dynamicPanel to see the GPS Sim
+			frame.setVisible(true);
 
-    if (segmentIDs.size() > 0)
-    {
-      if (mode == SET_ORIGIN)
-      {
-        dialog.setVisible(true);
-        originSegment = highlighted.get(segmentIDs.get(0));
-        System.out.println(originSegment + TAB + destinationSegment);
-      }
-      else if (mode == SET_DESTINATION)
-      {
-        dialog.setVisible(true);
-        destinationSegment = highlighted.get(segmentIDs.get(0));
-        System.out.println(originSegment + TAB + destinationSegment);
-      }
-    }
-  }
+			Geocoder geocoder = new Geocoder(geographicShapes, document, streets);
+			dialog = new GeocodeDialog(frame, geocoder);
+			dialog.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+			dialog.addStreetSegmentObserver(this);
+			dialog.setLocation((int) frame.getBounds().getMaxX(), (int) frame.getBounds().getY());
 
-  @Override
-  public void actionPerformed(final ActionEvent evt)
-  {
-    String ac = evt.getActionCommand();
+//      GPSSimulator gps = new GPSSimulator("rockingham.gps");
+//      InputStream is = gps.getInputStream();
 
-    if (ac.equals(ORIGIN))
-      mode = SET_ORIGIN;
-    else if (ac.equals(DESTINATION))
-      mode = SET_DESTINATION;
-    else if (ac.equals(CALCULATE))
-      mode = CALCULATE_PATH;
+			// Setup the GPSReaderTask
+//      GPSReaderTask gpsReader = new GPSReaderTask(is, "GPGGA");
+//      gpsReader.addGPSObserver(dynamicPanel);
+			frame.setVisible(true);
+//      gpsReader.execute();
 
-    if (ac.equals(ORIGIN) || ac.equals(DESTINATION))
-    {
-      if (!dialog.isVisible())
-      {
-        dialog.setLocation((int) frame.getBounds().getMaxX(), (int) frame.getBounds().getY());
-        dialog.setVisible(true);
-      }
-    }
-    
-    if(ac.equals(ASTAR)) {
-      pLabels = new PermanentLabelHeap(5, network.size());
-      alg = new AStar(pLabels);
-    }else if(ac.equals(BELLMANFORD)) {
-      cLabels = new CandidateLabelList(CandidateLabelList.NEWEST, network.size());
-      alg = new BellmanFord(cLabels);
-    }else if (ac.equals(DIJKSTRA)) {
-      pLabels = new PermanentLabelHeap(5, network.size());
-      alg = new LabelSettingAlgorithm(pLabels);
-    }else if (ac.equals(CORRECTING)) {
-      cLabels = new CandidateLabelList(CandidateLabelList.NEWEST, network.size());
-      alg = new LabelCorrectingAlgorithm(cLabels);
-    }
+			frame.setVisible(true);
+		} catch (IOException ioe) {
+			JOptionPane.showMessageDialog(frame, ioe.toString(), "Error", JOptionPane.ERROR_MESSAGE);
+		}
 
-    if (ac.equals(CALCULATE))
-    {
-      
-      // Construct the SwingWorker
-      task = new PathFindingWorker(alg, originSegment.getHead(), destinationSegment.getHead(),
-          network, document, panel);
-      task.addPropertyChangeListener(this);
-      task.shouldShowIntermediateResults(false); // TODO SET TO true IF YOU WANT TO SEE INTERMEDIATE
-                                                 // RESULTS
-      dialog.setVisible(false);
+	}
 
-      // Construct the dialog box
-      BackgroundTaskDialog<Map<String, StreetSegment>, String> btd = new BackgroundTaskDialog<Map<String, StreetSegment>, String>(
-          frame, "Calculating...", task);
+	@Override
+	public void propertyChange(final PropertyChangeEvent evt) {
+		if (evt.getPropertyName().equals("progress")) {
+			System.out.println("progress...");
+		} else if (evt.getPropertyName().equals("state")) {
+			if (evt.getNewValue().equals(SwingWorker.StateValue.DONE)) {
+				try {
+					Map<String, StreetSegment> path = task.get();
+					document.setHighlighted(path);
+					panel.repaint();
+					task = null;
+				} catch (InterruptedException | ExecutionException e) {
+					JOptionPane.showMessageDialog(frame, "Interrupted", "Exception", JOptionPane.ERROR_MESSAGE);
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 
-      // Execute
-      btd.execute();
-    }
+	@Override
+	public void handleStreetSegments(final List<String> segmentIDs) {
+		HashMap<String, StreetSegment> highlighted = new HashMap<String, StreetSegment>();
+		for (String id : segmentIDs) {
+			highlighted.put(id, document.getElement(id));
+		}
+		document.setHighlighted(highlighted);
+		panel.repaint();
 
-    if (ac.equals(EXIT))
-    {
-      dialog.dispose();
-      frame.dispose();
-      System.exit(0);
-    }
-  }
+		if (segmentIDs.size() > 0) {
+			if (mode == SET_ORIGIN) {
+				dialog.setVisible(true);
+				originSegment = highlighted.get(segmentIDs.get(0));
+				System.out.println(originSegment + TAB + destinationSegment);
+			} else if (mode == SET_DESTINATION) {
+				dialog.setVisible(true);
+				destinationSegment = highlighted.get(segmentIDs.get(0));
+				System.out.println(originSegment + TAB + destinationSegment);
+			}
+		}
+	}
+
+	@Override
+	public void actionPerformed(final ActionEvent evt) {
+		String ac = evt.getActionCommand();
+
+		if (ac.equals(ORIGIN))
+			mode = SET_ORIGIN;
+		else if (ac.equals(DESTINATION))
+			mode = SET_DESTINATION;
+		else if (ac.equals(CALCULATE))
+			mode = CALCULATE_PATH;
+
+		if (ac.equals(ORIGIN) || ac.equals(DESTINATION)) {
+			if (!dialog.isVisible()) {
+				dialog.setLocation((int) frame.getBounds().getMaxX(), (int) frame.getBounds().getY());
+				dialog.setVisible(true);
+			}
+		}
+
+		if (ac.equals(HEAP)) {
+			pLabels = new PermanentLabelHeap(5, network.size());
+			System.out.println("Data Structure set to heap");
+		} else if (ac.equals(LIST)) {
+			pLabels = new PermanentLabelList(network.size());
+			System.out.println("Data Structure set to list");
+		}
+
+		if (ac.equals(NEWEST)) {
+			cLabels = new CandidateLabelList(CandidateLabelList.NEWEST, network.size());
+			System.out.println("Policy set to newest");
+		} else if (ac.equals(OLDEST)) {
+			cLabels = new CandidateLabelList(CandidateLabelList.OLDEST, network.size());
+			System.out.println("Policy set to oldest");
+		}
+
+		if (ac.equals(ASTAR)) {
+			alg = new AStar(pLabels);
+			System.out.println("AStar is the algorithm");
+		} else if (ac.equals(BELLMANFORD)) {
+			alg = new BellmanFord(cLabels);
+			System.out.println("BellmanFord is the algorithm");
+		} else if (ac.equals(DIJKSTRA)) {
+			alg = new LabelSettingAlgorithm(pLabels);
+			System.out.println("Dijkstra is the algorithm");
+		} else if (ac.equals(CORRECTING)) {
+			alg = new LabelCorrectingAlgorithm(cLabels);
+			System.out.println("Label Correcting is the algorithm");
+		}
+
+		if (ac.equals(CALCULATE)) {
+
+			// Construct the SwingWorker
+			task = new PathFindingWorker(alg, originSegment.getHead(), destinationSegment.getHead(), network, document,
+					panel);
+			task.addPropertyChangeListener(this);
+			task.shouldShowIntermediateResults(false); // TODO SET TO true IF YOU WANT TO SEE INTERMEDIATE
+														// RESULTS
+			dialog.setVisible(false);
+
+			// Construct the dialog box
+			BackgroundTaskDialog<Map<String, StreetSegment>, String> btd = new BackgroundTaskDialog<Map<String, StreetSegment>, String>(
+					frame, "Calculating...", task);
+
+			// Execute
+			btd.execute();
+		}
+
+		if (ac.equals(EXIT)) {
+			dialog.dispose();
+			frame.dispose();
+			System.exit(0);
+		}
+	}
 
 }
--- FinalProject/src/graph/BellmanFord.java
+++ FinalProject/src/graph/BellmanFord.java
@@ -8,83 +8,69 @@ import feature.Intersection;
 import feature.StreetSegment;
 
 /**
- * Implementation of the BellmanFord Label setting algorithm for CS480 Final Project.
+ * Implementation of the BellmanFord Label setting algorithm for CS480 Final
+ * Project.
  * 
  * @author mason puckett
  * @version 1.0
  */
-public class BellmanFord extends AbstractShortestPathAlgorithm
-{
+public class BellmanFord extends AbstractShortestPathAlgorithm {
 
-  private CandidateLabelManager labels;
+	private CandidateLabelManager labels;
 
-  /**
-   * Constructor for a BellmanFord Label Setting algorithm.
-   * 
-   * @param labels
-   *          candidate label manager to use.
-   */
-  public BellmanFord(CandidateLabelManager labels)
-  {
-    super();
-    this.labels = labels;
-  }
+	/**
+	 * Constructor for a BellmanFord Label Setting algorithm.
+	 * 
+	 * @param labels candidate label manager to use.
+	 */
+	public BellmanFord(CandidateLabelManager labels) {
+		super();
+		this.labels = labels;
+	}
 
-  @Override
-  public Map<String, StreetSegment> findPath(int origin, int destination, StreetNetwork net)
-  {
-    Map<String, StreetSegment> result = new HashMap<String, StreetSegment>();
-    labels.getLabel(origin).setValue(0.0);
+	@Override
+	public Map<String, StreetSegment> findPath(int origin, int destination, StreetNetwork net) {
+		Map<String, StreetSegment> result = new HashMap<String, StreetSegment>();
+		labels.getLabel(origin).setValue(0.0);
 
-    int currID = origin;
-    processCandidate(currID, net);
-    do
-    {
-      List<Label> candidates = labels.getAllCandidates();
-      if (candidates == null)
-        break;
-      else
-      {
-        for (Label l : candidates)
-        {
-          processCandidate(l.getID(), net);
-        }
-      }
-    }
-    while (true);
-    
-    currID = destination;
-    while(currID != origin) {
-      StreetSegment segment = labels.getLabel(currID).getPredecessor();
-      if (segment != null)
-      {
-        result.put(segment.getID(), segment);
-        currID = segment.getTail();
-      }
-      else
-      {
-        currID = origin;
-      }
-    }
+		int currID = origin;
+		processCandidate(currID, net);
+		do {
+			List<Label> candidates = labels.getAllCandidates();
+			if (candidates == null)
+				break;
+			else {
+				for (Label l : candidates) {
+					processCandidate(l.getID(), net);
+				}
+			}
+		} while (true);
+		
+		currID = destination;
+		while (currID != origin) {
+			StreetSegment segment = labels.getLabel(currID).getPredecessor();
+			if (segment != null) {
+				result.put(segment.getID(), segment);
+				currID = segment.getTail();
+			} else {
+				currID = origin;
+			}
+		}
 
-    return result;
-  }
+		return result;
+	}
 
-  /**
-   * Helper method to update an id within the street network.
-   * 
-   * @param id
-   *          intersection to update
-   * @param net
-   *          streetnetwork to use
-   */
-  private void processCandidate(int id, StreetNetwork net)
-  {
-    Intersection current = net.getIntersection(id);
-    for (StreetSegment segment : current.getOutbound())
-    {
-      labels.adjustHeadValue(segment);
-    }
-  }
+	/**
+	 * Helper method to update an id within the street network.
+	 * 
+	 * @param id  intersection to update
+	 * @param net streetnetwork to use
+	 */
+	private void processCandidate(int id, StreetNetwork net) {
+		Intersection current = net.getIntersection(id);
+		for (StreetSegment segment : current.getOutbound()) {
+			labels.adjustHeadValue(segment);
+		}
+	}
 
 }
